810c58068281f019504e88a12a4bbaa6458e2009
parth patel <patelparth1973@gmail.com>
2021-09-22 14:11:04 +1000

    minor updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const authors = require("./routes/authors");
 
 app.use(express.json());
 
@@ -9,5 +10,5 @@ mongoose
   .then(() => console.log("connection sucessfull"))
   .catch((err) => console.log("connection failed", err));
 
-
+app.use("/api/authors", authors);
 app.listen(4000, () => console.log("running on port 4000"));
--- models/author.js
+++ models/author.js
@@ -6,13 +6,13 @@ const authorSchema = new mongoose.Schema({
   email: { type: String },
 });
 const Author = mongoose.model("Author", authorSchema);
-function validate(author) {
+function validateAuthor(author) {
   const schema = {
     name: Joi.string().required().min(3).max(25),
-    email: Joi.string().min(3).max(25),
+    email: Joi.string().min(3).max(25).trim(),
   };
   return Joi.validate(author, schema);
 }
 
 exports.Author = Author;
-exports.validate = validate;
+exports.validate = validateAuthor;
