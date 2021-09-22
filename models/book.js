415dc8f66b89ee987ec4ce03d731734d71a8c130
parth patel <patelparth1973@gmail.com>
2021-09-22 19:01:52 +1000

    modified book model

--- models/book.js
+++ models/book.js
@@ -2,15 +2,25 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 const { Author } = require("./author");
 
-const authorSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
   name: { type: String, required: true, min: 3 },
   author: { type: [mongoose.Schema.Types.ObjectId], ref: Author },
+  numberInStock: {
+    type: Number,
+    required: true,
+  },
+  rentalRate: {
+    type: Number,
+    required: true,
+  },
 });
-const Book = mongoose.model("Book", authorSchema);
+const Book = mongoose.model("Book", bookSchema);
 function validateBook(book) {
   const schema = {
     name: Joi.string().required().min(3).max(25),
     author: Joi.string().trim().required(),
+    numberInStock: Joi.number().required(),
+    rentalRate: Joi.number().required(),
   };
   return Joi.validate(book, schema);
 }
