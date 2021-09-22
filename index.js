1b1b57b6c266e7aea3a8b21e19dfef01d585c480
parth patel <patelparth1973@gmail.com>
2021-09-22 20:54:12 +1000

    added category routes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const authors = require("./routes/authors");
-
+const books = require("./routes/books");
+const category = require("./routes/categories");
 app.use(express.json());
 
 mongoose
@@ -11,4 +12,6 @@ mongoose
   .catch((err) => console.log("connection failed", err));
 
 app.use("/api/authors", authors);
+app.use("/api/books", books);
+app.use("/api/category", category);
 app.listen(4000, () => console.log("running on port 4000"));
